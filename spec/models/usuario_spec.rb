fbf7c59c86394779a365fee222ea34441cb294d8
Sergio Afonso <sergioafonsoga@gmail.com>
2013-11-28 16:50:31 +0000

    prueba para contraseña demasiada corta

--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -102,3 +102,8 @@ describe "when email format is valid" do
     it { should_not be_valid }
   end
 
+   describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
