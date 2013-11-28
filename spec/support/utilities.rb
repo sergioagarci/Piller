8272fd4a7c2a9fc3a380dbd0c40b18a2501c1372
tony <tony@VBox.(none)>
2013-11-28 17:37:56 +0000

    alerta error support

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,3 +12,11 @@ def valid_signin(usuario)
   fill_in "Password", with: usuario.password
   click_button "Acceder"
 end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+
