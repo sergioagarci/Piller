9113ab23f9ad2890d6cc202f30580614ea61a4ad
tony <tony@VBox.(none)>
2013-11-28 17:41:11 +0000

    test sign in

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -19,4 +19,11 @@ RSpec::Matchers.define :have_error_message do |message|
   end
 end
 
-
+def sign_in(usuario)
+  visit signin_path
+  fill_in "Email",    with: usuario.email
+  fill_in "Password", with: usuario.password
+  click_button "Acceder"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = usuario.remember_token
+end
