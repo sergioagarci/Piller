c2317cbe61a53b0e1831652e71c2d5884d5ea591
Sergio Afonso <sergioafonsoga@gmail.com>
2013-11-28 17:03:59 +0000

    implementando helper para usuarios

--- spec/helpers/usuarios_helper_spec.rb
+++ spec/helpers/usuarios_helper_spec.rb
@@ -2,5 +2,11 @@ require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
 # the UsuariosHelper. For example:
-
+describe UsuariosHelper do
+ # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
  end
