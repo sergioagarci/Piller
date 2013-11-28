9c8b44e33f3b1925d60c2fd3dafa6154ded75ef7
Sergio Afonso <sergioafonsoga@gmail.com>
2013-11-28 16:16:18 +0000

    Creando prueba para el registro de usuarios

---
+++ spec/models/usuario_spec.rb
@@ -0,0 +1,12 @@
+
+require 'spec_helper'
+
+describe Usuario do
+
+	before do
+    @user = Usuario.new(:username  => "User",
+              :email     => "user@example.com",
+              :password  => "example01",
+              :password_confirmation => "example01")
+  
+  end
