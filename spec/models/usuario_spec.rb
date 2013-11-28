f797dab8900dce028a6242df09082cc80971a334
Sergio Afonso <sergioafonsoga@gmail.com>
2013-11-28 16:47:27 +0000

    prueba para insercion de contraseña vacia

--- spec/models/usuario_spec.rb
+++ spec/models/usuario_spec.rb
@@ -83,7 +83,12 @@ describe "when email format is valid" do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
-    
+
+    it { should_not be_valid }
+  end
+
+   describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
 
