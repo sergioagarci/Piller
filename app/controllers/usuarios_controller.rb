231d51017e2d1d45428bd68c592b27ca64200136
tony <tony@VBox.(none)>
2013-12-16 16:34:37 +0000

    corrigiendo erratas en dir /app

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,7 +1,7 @@
 class UsuariosController < ApplicationController
-  before_action :signed_in_usuario, only: [:index, :edit, :update, :destroy]
-  before_action :correct_usuario,   only: [:edit, :update]
-  before_action :admin_usuario,     only: :destroy
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   def new
   	@usuario = Usuario.new
   end
@@ -9,7 +9,7 @@ class UsuariosController < ApplicationController
     @usuario = Usuario.find(params[:id])
   end
   def create
-    @usuario = Usuario.new(usuario_params)    # Not the final implementation!
+    @usuario = Usuario.new(user_params)    # Not the final implementation!
     if @usuario.save
       sign_in @usuario
       flash[:success] = "Welcome to Piller!"
@@ -31,7 +31,7 @@ class UsuariosController < ApplicationController
 
    def update
     @usuario = Usuario.find(params[:id])
-    if @usuario.update_attributes(usuario_params)
+    if @usuario.update_attributes(user_params)
       flash[:success] = "Profile updated"
       sign_in @usuario
       redirect_to @usuario
@@ -60,7 +60,7 @@ class UsuariosController < ApplicationController
 
   private
 
-    def usuario_params
+    def user_params
       params.require(:usuario).permit(:name, :email, :password,
                                    :password_confirmation)
     end
