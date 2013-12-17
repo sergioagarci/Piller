1049dc881bb44413f60df930c126575208669361
NoelDiazMesa <noeldiaz83@gmail.com>
2013-12-17 10:27:59 +0000

    Actualizando

--- app/controllers/microvideos_controller.rb
+++ app/controllers/microvideos_controller.rb
@@ -8,7 +8,7 @@ class MicrovideosController < ApplicationController
       redirect_to root_url
     else
       @feed_items = []
-      render 'static_pages/home'
+      render 'static_pages/home2'
     end
   end
 
